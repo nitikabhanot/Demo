cc60c089f9788c4172ab7847bca25fe7db4e4b04
Bhanot <nitika.bhanot@capgemini.com>
2018-02-13 12:49:38 +0530

    FirstCommit

---
+++ BankApplication/src/com/capgemini/exceptions/InSufficientAmountException.java
@@ -0,0 +1,10 @@
+package com.capgemini.exceptions;
+
+public class InSufficientAmountException extends Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
